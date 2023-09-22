8e314b505c262450bbef2d83654e4e77e6b18125
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-09-22 23:11:26 +0300

    FileEncrypter uygulaması.

---
+++ FileEncrypter/Decrypter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FileHasher
+{
+    internal class Decrypter
+    {
+        public void Run()
+        {
+            string content = InputHelpers.GetFileContent("Enter the file path you want to encrypt:", out string path);
+
+            var encoding = Encoding.UTF8;
+            PasswordGetter.GetKey(out Byte[] password, 8, 16, encoding);
+
+            EncryptedData data = JsonSerializer.Deserialize<EncryptedData>(content) ?? new EncryptedData();
+
+            if (String.IsNullOrEmpty(data.Salt))
+            {
+                Console.WriteLine("inconsistn file! Cancelled");
+            }
+            if (data.Content?.Length == null || data.InitializationVector?.Length == null)
+            {
+                Console.WriteLine("inconsistn file! Cancelled");
+            }
+
+            string salt = data.Salt;
+            var key = PasswordGetter.CreateKey16(password, salt, encoding);
+            var IV = data.InitializationVector;
+            string plaintext = string.Empty;
+            using (Aes aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(key, IV))
+                {
+                    using (MemoryStream msDecrypt = new MemoryStream(data.Content))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                            {
+
+                                // Read the decrypted bytes from the decrypting stream
+                                // and place them in a string.
+                                plaintext = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+            string directory = Path.GetDirectoryName(path) ?? String.Empty;
+            string fileName = Path.GetFileNameWithoutExtension(path) + "_decrypted.txt";
+            string outputFile = Path.Combine(directory, fileName);
+            File.WriteAllText(outputFile, plaintext);
+        }
+    }
+}
---
+++ FileEncrypter/Encrypter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FileHasher
+{
+    internal class Encrypter
+    {
+        public Encrypter()
+        {
+        }
+
+        public void Run()
+        {
+            string content = InputHelpers.GetFileContent("Enter the file path you want to encrypt:", out string path);
+
+            var encoding = Encoding.UTF8;
+            PasswordGetter.GetKey(out Byte[] password, 8, 16, encoding);
+
+            string salt = "jnaljfzh";
+            byte[] encrypted;
+            byte[] key = new byte[16];            
+            key = PasswordGetter.CreateKey16(password, salt, encoding);
+
+            EncryptedData encryptedData = new EncryptedData();
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                encryptedData.InitializationVector = aesAlg.IV;
+                encryptedData.Salt = salt;
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            sw.Write(content);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                        encryptedData.Content = encrypted;
+                    }
+                }
+            }
+            string directory = Path.GetDirectoryName(path) ?? string.Empty;
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string OutputFile = Path.Combine(directory, $"{fileName}_encrypted");
+            //using (FileStream fs = File.Open(OutputFile, FileMode.Create))
+            //{
+            //        fs.Write(encrypted);
+            //}
+            string jsonFileName = OutputFile + ".json";
+            string str = JsonSerializer.Serialize(encryptedData);
+            File.WriteAllText(jsonFileName, str);
+
+            Console.WriteLine("file is written to " + OutputFile);
+        }
+    }
+}
